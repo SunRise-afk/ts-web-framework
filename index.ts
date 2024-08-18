fa6284996d8a5165311a8b36fc37beca424a4813
Illia Ilin <ilin.i.1337@gmail.com>
2024-08-18 16:53:19 +0200

    feature/APP-Create-user-model

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log('Thank you for using WebStorm')
+import { User } from "./src/models/User";
+
+const user = new User({name: "myname", age: 26})
+
+console.log(user.get("name"));
+console.log(user.get("age"));
