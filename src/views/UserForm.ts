5b6092a5502ec66d28801944e572ce3649fc4bcc
Illia Ilin <ilin.i.1337@gmail.com>
2024-08-21 22:59:00 +0200

    feature/VIEW-Implement-user-form-view
    - Implement events binding functionality for user form elements

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,18 +1,45 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  onButtonClick = () => {
+    console.log("button click");
+  };
+  onHeaderOver = () => {
+    console.log("hover over header");
+  };
+
+  eventsMap = (): { [key: string]: () => void } => {
+    return {
+      "click:button": this.onButtonClick,
+      "mouseenter:h1": this.onHeaderOver,
+    };
+  };
+
   template = (): string => {
     return `
         <div>
             <h1>User Form</h1>
             <input/>
+            <button>Click</button>
         </div>
         `;
+  };
+
+  bindEvents = (fragment: DocumentFragment): void => {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [name, selector] = eventKey.split(":");
+      fragment
+        .querySelectorAll(selector)
+        .forEach((item) => item.addEventListener(name, eventsMap[eventKey]));
+    }
   };
 
   render = (): void => {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     document.body.appendChild(templateElement.content);
   };
 }
